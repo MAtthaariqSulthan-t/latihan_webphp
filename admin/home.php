f4167cb830e86dde241fe696016e37ab28a45923
MAtthaariqSulthan-t <yogaperdana78@gmail.com>
2022-03-29 20:26:46 +0700

    update

--- admin/home.php
+++ admin/home.php
@@ -4,22 +4,54 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="i.css"/>
+    <title>Admin Lock</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
-  <!-- navbar -->
-  <header>
-    <nav>
-      <a href="">Belajar php</a>|
-      <a href="">Sign In</a>
+<!-- header -->
+ <header>
+   <nav class="bg-dark navbar navbar-expand-lg">
+    <a href="" class="navbar-brand m-2 px-3">HEADER</a>
+      <div class="navbar-nav">
+        <div class="nav-item">
+          <a class="nav-link px-3" href="">Sign out</a>
+        </div>
+      </div>
     </nav>
   </header>
-  <!-- lef and content -->
-  <div>
-    <div>
-      trait_exists
+<!-- end header -->
+<!-- left menu -->
+  <div class="container-fluid">
+    <div class="row">
+      <nav id="leftmenu" class="col-lg-2">
+        <div class="">
+          <?php
+            $menu= array(
+              array("id"=>"01", "nm_menu"=>"dashboard", "link"=> "#"),
+              array("id"=>"02", "nm_menu"=>"blog", "link"=> "#"),
+              array("id"=>"03", "nm_menu"=>"berita", "link"=> "mod_berita")
+            );
+            foreach($menu as $m){
+          ?>
+          <ul class="list-group list-group-flush">
+            <!-- <li class="list-group-item"><//?= $m["id"] ?></li> -->
+            <li class="list-group-item"><a class="nav-link" aria-current="page" href="<?= $m["link"] ?>" onclick="openpage()"><?= $m["nm_menu"] ?></a></li>
+            <!-- <li class="list-group-item"><//?= $m["link"] ?></li> -->
+          </ul>
+          <?php } ?>
+        </div>
+      </nav>
+<!-- content -->
+  <main class="ms-sm-auto col-lg-10">
+    <div class="justify-content-between alight-item-center pt-3 ">
+      <h4>Content</h4>
+    </div>
+    <div id="content">control here</div>
+  </main>
     </div>
   </div>
+<!-- script js -->
+<script src="javascript/main.js"></script>
 </body>
 </html>
---
+++ admin/javascript/main.js
@@ -0,0 +1,3 @@
+function openpage(page) {
+  document.getElementById("content").innerHTML = '<object type="text/html" data="' + page + '"  width="1000" height="1000"></object>';
+}
